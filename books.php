cd8b9875e1138a3605572f0612b667b9870cf5cd
Romela Azizyan <romelaazizyan@mail.weber.edu>
2021-01-19 18:57:55 -0700

    assignment 2

--- books.php
+++ books.php
@@ -2,4 +2,4 @@
     header("Access-Control-Allow-Origin: *");
 ?>
 
-{"books-read":[{"Title":"The Hobbit","Author":"J. R. R. Tolkien"},{"Title":"The Body", "Author":"Bill Bryson"}]}
+{"books-read":[{"Title":"The Hobbit","Author":"J. R. R. Tolkien"},{"Title":"The Hunger Games", "Author":"Suzanne Collins"},{"Title":"Michael Vey: The Prisoner of Cell 25", "Author":"Richard Paul Evans"}]}
